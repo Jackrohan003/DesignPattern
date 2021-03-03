9bba2f0b2201d79c36d29d429cd220dfe0cb3927
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2021-03-03 13:45:31 +0530

    Singleton Design Pattern Using Best-Practices

---
+++ Singleton/Src/com/App.java
@@ -0,0 +1,18 @@
+package com.singleton;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+		LazzyInnerClassSingleton obj1 = LazzyInnerClassSingleton.getInstance();
+
+		System.out.println(obj1.hashCode());
+
+		LazzyInnerClassSingleton obj2 = LazzyInnerClassSingleton.getInstance();
+
+		System.out.println(obj2.hashCode());
+	}
+
+}
